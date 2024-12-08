316b5ec1b3bb96ccb00cc01eb2f411361b9d9980
Tereshchenko Ekaterina <tereshchenko.trin@gmail.com>
2024-12-08 17:37:26 +0300

    feat(init): progect start

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import '@/styles/app.scss'
+
+export function App() {
+  return (<h1>Start progect</h1>)
+}
+
