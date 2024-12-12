5ff8321f95605582901890bf6232d284107e0684
Tereshchenko Ekaterina <tereshchenko.trin@gmail.com>
2024-12-12 21:25:21 +0300

    feat(style): added films style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,12 @@
+import { className } from '@/utils/className'
+
 export function Card(props) {
+  const ratingContainerClassName = `card__rating-container ${className ({
+    'card__rating-container_green': props.ratingKinopoisk >= 7.5,
+    'card__rating-container_yellow': props.ratingKinopoisk < 7.5 && props.ratingKinopoisk >= 5,
+    'card__rating-container_orange': props.ratingKinopoisk < 5,
+  })}`
+
   function renderTitle(props) {
     if (props.nameOriginal) {
       return <p className="card__title">{props.nameOriginal}</p>
@@ -9,13 +17,20 @@ export function Card(props) {
 
   return (
     <div className="card">
-      <div className="card__image">
-        <div className="card__rating">{props.ratingKinopoisk}</div>
-        <img src={props.posterUrl} alt="poster" />
+      <div className="card__poster">
+        <div className={ratingContainerClassName}
+        >
+          <p className="card__rating">{props.ratingKinopoisk}</p>
+        </div>
+        <img className="card__image" src={props.posterUrl} alt="poster" />
       </div>
-      <div className="card__content">
-        {renderTitle(props)}
-        {/* <p className="card__">{props.genres}</p> */}
+      <div className="card__text">
+        <div className="card__title-container">
+          {renderTitle(props)}
+        </div>
+        <div className="card__genres-container">
+          {/* <p className="card__genres">{props.genres}</p> */}
+        </div>
       </div>
     </div>
   )
